a8a34e69dab529f7ab8b88dd9251330d2302ea85
d4rent <d4rentseanquinn@gmail.com>
2025-08-28 05:12:45 +0100

    Pipedream - {{steps.trigger.context.workflow_name}} ({{steps.trigger.context.workflow_id}})

---
+++ apps/web/src/app/providers.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#E6F6FF',
+      100: '#BAE3FF',
+      200: '#7CC4FA',
+      300: '#47A3F3',
+      400: '#2186EB',
+      500: '#0967D2',
+      600: '#0552B5',
+      700: '#03449E',
+      800: '#01337D',
+      900: '#002159',
+    },
+  },
+  fonts: {
+    heading: 'var(--font-inter)',
+    body: 'var(--font-inter)',
+  },
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: 'brand',
+      },
+    },
+  },
+})
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  )
+}
